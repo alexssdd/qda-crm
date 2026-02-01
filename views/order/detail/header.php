368e1793ff12c2e1937a168f345b8eaf71b8b73b
alexssdd <mr.alex.ssdd@gmail.com>
2026-02-01 17:51:30 +0500

    Order detail

--- views/order/detail/header.php
+++ views/order/detail/header.php
@@ -13,14 +13,6 @@ use app\modules\order\helpers\OrderHelper;
     <div class="order-header__left">
         <div class="order-header__items">
             <div class="order-header__item">
-                <span class="order-header__label">Source ID:</span>
-                <span class="order-header__value"><?= $order->source_id ?></span>
-            </div>
-            <div class="order-header__item">
-                <span class="order-header__label">Source Number:</span>
-                <span class="order-header__value"></span>
-            </div>
-            <div class="order-header__item">
                 <span class="order-header__label">Номер:</span>
                 <span class="order-header__value"><?= $order->number ?></span>
             </div>
@@ -28,18 +20,18 @@ use app\modules\order\helpers\OrderHelper;
                 <span class="order-header__label">Дата:</span>
                 <span class="order-header__value"><?= OrderHelper::getCreated($order) ?></span>
             </div>
+            <div class="order-header__item">
+                <span class="order-header__label">Source ID:</span>
+                <span class="order-header__value"><?= $order->source_id ?></span>
+            </div>
+            <div class="order-header__item">
+                <span class="order-header__label">Source Number:</span>
+                <span class="order-header__value"></span>
+            </div>
         </div>
     </div>
     <div class="order-header__right">
         <div class="order-header__items">
-            <div class="order-header__item">
-                <span class="order-header__label"></span>
-                <span class="order-header__value"></span>
-            </div>
-            <div class="order-header__item">
-                <span class="order-header__label"></span>
-                <span class="order-header__value"></span>
-            </div>
         </div>
         <div class="order-header__time"></div>
     </div>
