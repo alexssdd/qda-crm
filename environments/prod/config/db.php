60c787573fbcee8c527774721d47e15c5d025051
alexssdd <mr.alex.ssdd@gmail.com>
2026-07-28 04:25:44 +0700

    fix CRM environment database templates [arch-ok]

--- environments/prod/config/db.php
+++ environments/prod/config/db.php
@@ -3,9 +3,13 @@ return [
     'db' => [
         'class' => 'yii\db\Connection',
         'charset' => 'utf8mb4',
-        'dsn' => 'mysql:host=localhost;dbname=app_db',
-        'username' => 'app_db',
-        'password' => '',
-        'tablePrefix' => 'ow_'
+        // CRM и Global используют одну БД на этой же ноде.
+        // localhost подключает MariaDB через unix socket.
+        'dsn' => 'mysql:host=localhost;dbname=global_db',
+        'username' => 'global_app',
+        // `php init` копирует файл без подстановки переменных.
+        // После init заменить placeholder в config/db.php реальным паролем.
+        'password' => '<global-app-password>',
+        'tablePrefix' => 'ow_',
     ],
 ];
