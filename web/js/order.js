dd9d4cd386da5fcb8ed1211260541dfa959cbd13
alexssdd <mr.alex.ssdd@gmail.com>
2026-07-28 02:27:11 +0700

    style rebalance CRM order timer [arch-ok]

--- web/js/order.js
+++ web/js/order.js
@@ -247,7 +247,7 @@ window.Order = {
             countdown: false,
             seconds: seconds,
             displayDays: 2,
-            fontSize: 20,
+            fontSize: 24,
             fontFamily: 'tahoma, arial, verdana, sans-serif'
         });
         if (stop){
